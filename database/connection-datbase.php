e92c43986b7a591ad79497bfe584946cdda2e562
Hady M. Morsy <72827484+HadyMohamedMorsy@users.noreply.github.com>
2022-04-29 12:48:15 +0200

    add

--- database/connection-datbase.php
+++ database/connection-datbase.php
@@ -11,9 +11,9 @@
         public function Connect(){
 
             $this->servername = 'localhost';
-            $this->username = 'root';
-            $this->password = '';
-            $this->dbname = 'chat';
+            $this->username = 'u590527764_HMM_PDT_1';
+            $this->password = '#AbdulBaset01097579845Hady#';
+            $this->dbname = 'u590527764_HMM_PDT_1';
         
             // Create connection
             $conn = new mysqli($this->servername,$this->username, $this->password,$this->dbname );

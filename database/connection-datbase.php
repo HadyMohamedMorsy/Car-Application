fa2cf500e56f3599f0968b1d8e4b929b143ca029
AbdulBaset R. Sayed <74756037+AbdulBasetRS@users.noreply.github.com>
2022-04-29 13:16:18 +0200

    update

--- database/connection-datbase.php
+++ database/connection-datbase.php
@@ -10,10 +10,11 @@
 
         public function Connect(){
 
-            $this->servername = 'localhost';
+            $this->servername = 'sql778.main-hosting.eu';
             $this->username = 'u590527764_HMM_PDT_1';
             $this->password = '#AbdulBaset01097579845Hady#';
             $this->dbname = 'u590527764_HMM_PDT_1';
+            
         
             // Create connection
             $conn = new mysqli($this->servername,$this->username, $this->password,$this->dbname );
@@ -21,4 +22,5 @@
             return $conn;
         }
     }
+
 ?>
